91071cf0a9a2289f439a285cdf60f9ac47e3ddbf
gordo1998 <jordibarreda98@gmail.com>
2024-06-28 09:44:39 +0200

    n

--- demo/src/main/java/controller/UsersC.java
+++ demo/src/main/java/controller/UsersC.java
@@ -1,5 +1,32 @@
 package controller;
 
-public class UsersC {
+import java.util.Map;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import service.UsersImpS;
+
+@RestController
+public class UsersC {
+	
+	UsersImpS sUsers = new UsersImpS();
+	
+	@PostMapping(value = "register", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public Map<String, Object> registerUser(@RequestBody Map<String, Object> user) {
+		return sUsers.addUser((String)user.get("email"), (String)user.get("password"));
+	}
+	
+	@GetMapping(value = "login", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public Map<String, Object> loginUser(@RequestBody Map<String, Object> user){
+		return sUsers.existUserService((String)user.get("email"), (String)user.get("password"));
+	}
+	
+	
+	
+	
+	
 }
